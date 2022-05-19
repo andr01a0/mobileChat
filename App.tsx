5f51e1a80fc85267dbc808d5e06bdb64fb400e4f
André Bittencourt <andr01a0@stud.kea.dk>
2022-05-19 21:37:54 +0200

    create user firebase

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, Text, Button, View } from 'react-native';
 import { auth } from './src/configs/firebase'
 import { createUserWithEmailAndPassword as createUser } from "firebase/auth";
 
@@ -9,13 +9,35 @@ function createUEP(email: string, password: string) {
     const user = userCredential.user;
     console.log(user);
   });
-}
+};
 
 export default function App() {
-
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <View style={styles.container}>
       <Text style={styles.text}>hello friend. 🤖</Text>
+      <TextInput
+        style={{height: 40}}
+        placeholder="Insert email"
+        onChangeText={newEmail => setEmail(newEmail)}
+        defaultValue={email}
+        keyboardType="email-address"
+        autoCapitalize='none'
+      />
+      <TextInput
+        style={{height: 40}}
+        placeholder="Insert password"
+        onChangeText={newPassword => setPassword(newPassword)}
+        defaultValue={password}
+        secureTextEntry={true}
+      />
+      <Button
+        onPress={() => {
+          createUEP(email, password);
+        }}
+        title="Create User"
+      />
       <StatusBar style="auto" />
     </View>
   );
--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,13 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import {
-  API_KEY,
-  AUTH_DOMAIN,
-  PROJECT_ID,
-  STORAGE_BUCKET,
-  MESSAGING_SENDER_ID,
-  APP_ID
-} from "@env"
+  API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET,
+  MESSAGING_SENDER_ID, APP_ID
+} from "@env";
 
 // Your app's Firebase configuration
 const firebaseConfig = {
@@ -24,30 +20,3 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
-
-/*
-export const createUser = (email, password) => {
-  createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    console.log(user);
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
-}
-
-export const signIn = (email, password) => {
-  signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    console.log(user);
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
-}*/
