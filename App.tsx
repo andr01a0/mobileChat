59f9fdda0df5f8f7e1e1c41be56484678683c46c
André Bittencourt <andr01a0@stud.kea.dk>
2022-05-26 12:21:46 +0200

    navigator

--- App.tsx
+++ App.tsx
@@ -1,56 +1,18 @@
-import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, TextInput, Text, Button, View } from 'react-native';
-import { auth } from './src/configs/firebase'
-import { createUserWithEmailAndPassword as createUser } from "firebase/auth";
+import React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen';
 
-function createUEP(email: string, password: string) {
-  createUser(auth, email, password).then((userCredential) => {
-    const user = userCredential.user;
-    console.log(user);
-  });
-};
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>hello friend. 🤖</Text>
-      <TextInput
-        style={{height: 40}}
-        placeholder="Insert email"
-        onChangeText={newEmail => setEmail(newEmail)}
-        defaultValue={email}
-        keyboardType="email-address"
-        autoCapitalize='none'
-      />
-      <TextInput
-        style={{height: 40}}
-        placeholder="Insert password"
-        onChangeText={newPassword => setPassword(newPassword)}
-        defaultValue={password}
-        secureTextEntry={true}
-      />
-      <Button
-        onPress={() => {
-          createUEP(email, password);
-        }}
-        title="Create User"
-      />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 40,
-  },
-});
