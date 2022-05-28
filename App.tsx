73ebea2976f0e570013aa6b37aaf159001addf73
André Bittencourt <andr01a0@stud.kea.dk>
2022-05-28 13:23:10 +0200

    typed hooks

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Provider } from 'react-redux'
-import store from './src/redux/store'
+import { store } from './src/redux/store'
 import HomeScreen from './src/screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
