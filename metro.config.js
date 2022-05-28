76c673aabfeeb88322e2e4b9a10f811f4d8748ed
André Bittencourt <andr01a0@stud.kea.dk>
2022-05-28 19:36:49 +0200

    metro transformer drop_console

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,8 @@ const { getDefaultConfig } = require('@expo/metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
 
+defaultConfig.transformer.minifierConfig.compress.drop_console = true;
+
 defaultConfig.resolver.assetExts.push('cjs');
 
 module.exports = defaultConfig;
