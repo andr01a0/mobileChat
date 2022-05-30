e1113098c98a34e59b60af0d54616b8db80edf56
André Bittencourt <andr01a0@stud.kea.dk>
2022-05-30 05:43:13 +0200

    styling

--- src/screens/AuthScreen.tsx
+++ src/screens/AuthScreen.tsx
@@ -1,13 +1,25 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 
 export default function AuthScreen({ navigation }: any) {
 	
 	return (
-		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Authentication Screen</Text>
+		<View style={styles.container}>
+      <Text style={styles.text}>Authentication Screen</Text>
 			<Button title="Sign Up" onPress={() => navigation.navigate('SignUp')} />
 			<Button title="Sign In" onPress={() => navigation.navigate('SignIn')} />
     </View>
 	);
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 40,
+  },
+});
--- src/screens/SignInScreen.tsx
+++ src/screens/SignInScreen.tsx
@@ -1,11 +1,26 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function HomeScreen({ navigation }: any) {
 	
 	return (
-		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Sign In Screen</Text>
+		<View style={styles.container}>
+      <Text style={styles.text}>Sign In Screen</Text>
     </View>
 	);
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 40,
+  },
+	textInput: {
+		height: 50,
+	},
+});
