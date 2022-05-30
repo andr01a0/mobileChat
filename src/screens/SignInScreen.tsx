058b28ab9b364e9eea596fc808548a798c2638d9
André Bittencourt <andr01a0@stud.kea.dk>
2022-05-30 05:46:30 +0200

    authentication screens

--- src/screens/SignInScreen.tsx
+++ src/screens/SignInScreen.tsx
@@ -1,11 +1,35 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import * as SecureStore from 'expo-secure-store';
 
-export default function HomeScreen({ navigation }: any) {
+export default function SignIn({ navigation }: any) {
+	const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+	const _signInAsync = async () => {
+    await SecureStore.setItemAsync('userToken', 'abc');
+    navigation.navigate('App');
+  };
 	
 	return (
 		<View style={styles.container}>
-      <Text style={styles.text}>Sign In Screen</Text>
+			<Text style={styles.text}>Sign Up Screen</Text>
+			<TextInput
+				style={styles.textInput}
+				placeholder="Insert email"
+				onChangeText={newEmail => setEmail(newEmail)}
+				defaultValue={email}
+				keyboardType="email-address"
+				autoCapitalize='none'
+			/>
+			<TextInput
+				style={styles.textInput}
+				placeholder="Insert password"
+				onChangeText={newPassword => setPassword(newPassword)}
+				defaultValue={password}
+				secureTextEntry={true}
+			/>
+      <Button title="Sign In" onPress={_signInAsync} />
     </View>
 	);
 }
